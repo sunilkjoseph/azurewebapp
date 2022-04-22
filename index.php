c7790209219c9c06a210b8ac34ccd8aeb26f1ed0
sunilkjoseph <104191368+sunilkjoseph@users.noreply.github.com>
2022-04-22 13:08:23 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>My Azure Web App</title>
+</head>
+<body>
+<h1>Welcome to my Azure Web App</h1>
+<?php
+echo "<h1>FDP ASAP KERALA</h1>";
+?>
+</body>
+</html>
