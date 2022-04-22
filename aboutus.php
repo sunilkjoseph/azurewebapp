922f4dc2ed9fd9fb547f4edaca51b1f7d76c877c
sunilkjoseph <104191368+sunilkjoseph@users.noreply.github.com>
2022-04-22 13:26:06 +0530

    Create aboutus.php

---
+++ aboutus.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>About Us</title>
+</head>
+<body>
+<h1>About ASAP FDP</h1>
+<?php
+echo "<h1> CLOUD COMPUTING FDP AT ASAP Kerala</h1>";
+?>
+  <a href="index.php">Home</a>
+</body>
+</html>
